b954a7f3cebcaf1d3449e586d6a8263ca925c568
Jason Lee <huacnlee@gmail.com>
2016-03-09 16:47:10 +0800

    Add more hosts

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-# server 'us0.gems.ruby-china.org', user: 'ubuntu', roles: %w{app}
-server '49.51.10.107', user: 'ubuntu', roles: %w{app}
+server 'us0.gems.ruby-china.org', user: 'ubuntu', roles: %w{app}
+server 'us1.gems.ruby-china.org', user: 'ubuntu', roles: %w{app}

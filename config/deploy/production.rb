dede11f64205dc97fd7c6e990ea34b34a3e83ff9
Jason Lee <huacnlee@gmail.com>
2016-03-09 15:56:02 +0800

    Fix config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,2 @@
-server 'us0.gems.ruby-china.org', user: 'ubuntu', roles: %w{app}
+# server 'us0.gems.ruby-china.org', user: 'ubuntu', roles: %w{app}
+server '49.51.10.107', user: 'ubuntu', roles: %w{app}
